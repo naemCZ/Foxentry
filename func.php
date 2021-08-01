eb56cdee8cecdd83dc4316378127520d85655df2
naemCZ <68488827+naemCZ@users.noreply.github.com>
2021-08-01 18:43:16 +0200

    Add files via upload

---
+++ func.php
@@ -0,0 +1,187 @@
+<?php
+
+use Elasticsearch\ClientBuilder;
+require 'vendor/autoload.php';
+
+
+function indexSite($url, $elementType, $elementText){
+
+    $client = ClientBuilder::create()->build();
+    $indexName = transformUrlToIndexName($url);
+
+    createIndex($indexName, $client);
+
+    $dom = getDom($url);
+        
+    $element = findElement($dom, $elementType, $elementText);
+    $array = transformToArray(getNextSiblingNode($element), $element->nodeName, $elementText);
+        
+    foreach ($array as $i=>$row){
+        $params = [
+            'index' => $indexName,
+            'id' => $i,
+            'body' => $row
+        ];
+        try{
+            $response = $client->index($params);
+        } catch (Exception $e) {
+            echo 'Caught exception: ',  $e->getMessage(), "\n";
+        } 
+    }
+}
+    
+function getDom($url){
+    $doc = new DOMDocument();
+    $doc->loadHTMLFile($url);
+    return $doc;
+}
+
+function createIndex($indexName, $client){
+
+    $params = ['index' => $indexName];
+    if ($client->indices()->exists($params)){
+        return;
+    }
+
+    $params = [
+        'index' => $indexName,
+        'body' => [
+            'settings' => [
+                'number_of_shards' => 1,
+                'number_of_replicas' => 1
+            ],
+            'mappings' => [
+                '_source' => [
+                    'enabled' => true
+                ],
+                'properties' => [
+                    'header' => [
+                        'type' => 'keyword'
+                    ],
+                    'text' => [
+                        'type' => 'text'
+                    ],
+                    'section' => [
+                        'type' => 'keyword'
+                    ]
+                ]
+            ]
+        ]
+    ];
+
+    try {
+        $response = $client->indices()->create($params);
+    } catch (Exception $e) {
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+    } 
+
+}
+
+function transformUrlToIndexName($url){
+    $url = strtolower($url);
+    if (substr($url, -1) == "/"){
+        $url = substr($url, 0, -1);
+    }
+    
+    $indexName = str_replace("https://", "", $url);
+    $indexName = str_replace("http://", "", $indexName);
+    $indexName = str_replace("/", "-", $indexName);
+    
+    return $indexName;
+}
+
+function findElement($dom, $elementType, $elementText){
+    $elementText = strtolower($elementText);
+    if (strtolower($elementType) != "h"){
+        foreach($dom->getElementsByTagName($elementType) as $element){
+            $currentElementText = strtolower($element->nodeValue);
+            if (strpos($currentElementText, $elementText)){
+                return $element;
+            }
+        }
+    }else{
+        $headerNum = 1;
+        $maxHeaderNum = 6;
+        
+        while ($headerNum <= $maxHeaderNum){
+            $elementType = "h".$headerNum;
+            foreach($dom->getElementsByTagName($elementType) as $element){
+                $currentElementText = strtolower($element->nodeValue);
+                if (strpos($currentElementText, $elementText)){
+                    return $element;
+                }
+            }
+            $headerNum++;
+        }
+    }
+    
+    echo "Element not found";
+}
+
+function transformToArray($element, $elementType, $sectionName){
+    //TODO - elementType <> hXX
+    $sectionName = getSectionNameFromText($sectionName);
+    
+    $headerNum = substr($elementType, -1);
+    $headerNum++;
+    $elementType = "h".$headerNum;
+    $i=0;
+
+    foreach($element->getElementsByTagName($elementType) as $child){
+        $returnArray[$i] ['header'] = $child->nodeValue;
+        $returnArray[$i] ['text'] = getNextSiblingNode($child)->nodeValue;
+        $returnArray[$i] ['section'] = $sectionName;
+        $i++;
+    }
+
+    return $returnArray;
+}
+
+function getNextSiblingNode($node)
+{
+    while ($node && ($node = $node->nextSibling)) {
+        if ($node instanceof DOMElement) {
+            break;
+        }
+    }
+    return $node;
+}
+
+function getSectionNameFromText($text){
+   return str_replace(" ", "-", $text); 
+}
+
+function getIndexData($url, $sectionName ){
+    $client = ClientBuilder::create()->build();
+    
+    $indexName = transformUrlToIndexName($url);
+    $sectionName = getSectionNameFromText($sectionName);
+
+    $params = [
+        'index' => $indexName,
+        'from' => 0,
+        'size' => 100,
+        'body' => [
+            'query' => [
+                'match' => [
+                    'section' => $sectionName
+                ]
+            ]
+        ]
+    ];
+    
+    try {
+        $response = $client->search($params);
+    } catch (Exception $e) {
+        echo 'Caught exception: ',  $e->getMessage(), "\n";
+    } 
+    
+    foreach($response['hits']['hits'] as $i=>$record){
+        $returnArray[$i]['header'] = $record['_source']['header'];
+        $returnArray[$i]['text'] = $record['_source']['text'];
+    }
+
+    return $returnArray;
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,53 @@
+﻿<?php
+    include 'func.php';
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <meta charset=utf-8>
+        <title>Foxentry test</title>
+    </head>
+    <body>
+        
+        <?php
+            if (!$_POST || $_POST['url'] == "" || $_POST['elementText'] == "" ) 
+            {
+        ?>
+            <form method="post">
+                <label>Vložte url</label>
+                <input type = "text" name = "url" value="<?php echo $_POST['url']; ?>"><br>
+                <label>Vložte text nadpisu</label>
+                <input type = "text" value ="<?php echo $_POST['elementText']; ?>" name ="elementText"><br>
+                <input type="submit"value = "Odeslat">
+            </form>
+        <?php
+            }else{
+               indexSite($_POST['url'], "h", $_POST['elementText']); 
+               $data = getIndexData($_POST['url'], $_POST['elementText']);
+               
+            
+        ?>
+            <table>
+                <tr>
+                    <th>Titulek</th>
+                    <th>Text</th>
+                </tr>
+                <?php
+                foreach ($data as $row)
+                {
+                ?>
+                    <tr>
+                        <td><?php echo $row['header']; ?> </td>
+                        <td><?php echo $row['text']; ?> </td>
+                    </tr>
+            <?php
+                }
+            ?>
+            </table>
+            <a href = "index.php">Návrat na formulář</a>
+        <?php
+            }
+        ?>
+    </body>
+</html>
